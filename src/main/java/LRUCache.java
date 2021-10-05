614775bbfbbfa81bfc5f60996058563b8c24faad
liuyuanyuan <675055829@qq.com>
2021-10-05 18:32:32 +0800

    lru

---
+++ src/main/java/LRUCache.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class LRUCache {
+    /**
+     * 面试题 16.25. LRU 缓存
+     */
+    int capacity;
+    List<int
+
+    public LRUCache(int capacity) {
+
+    }
+
+    public void put(int key,int value){
+
+    }
+
+    public int get(int key){
+
+    }
+}
